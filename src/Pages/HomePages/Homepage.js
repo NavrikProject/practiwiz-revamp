6a31d507571adb40eefe394ba89c4675d3985e03
Mahesh <b.mahesh311296@gmail.com>
2024-06-07 12:34:30 +0530

    force

--- src/Pages/HomePages/Homepage.js
+++ src/Pages/HomePages/Homepage.js
@@ -3,7 +3,7 @@ import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
 import HomePageBanner from "../../Components/Home/HomePageBanner";
 import Section2 from "../../Components/Home/Section2";
-import Section3 from "../../Components/Home/Section3";
+// import Section3 from "../../Components/Home/Section3";
 
 const Homepage = () => {
   return (
