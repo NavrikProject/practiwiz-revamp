e13a6a57159263c398ab93b9fc7aab6e6ca89356
Gagan-V4 <Gaganverma124567@gmail.com>
2024-07-18 11:03:11 +0530

    changes in the login page

--- src/Components/Forms/Login/LoginForm.js
+++ src/Components/Forms/Login/LoginForm.js
@@ -7,9 +7,10 @@ const LoginForm = () => {
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
-    trigger,
-  } = useForm();
+  } = useForm()
+  const onSubmit = (data) => console.log(data)
 
   const [type, setType] = useState("member");
   const [error, setError] = useState("");
@@ -54,14 +55,14 @@ const LoginForm = () => {
   };
   return (
     <main>
-      <div class="regis_background " id="loginBg">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-6 mb-4">
-              <div class="iuhieiuihaw_left sticky-top">
+      <div className="regis_background " id="loginBg">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 mb-4">
+              <div className="iuhieiuihaw_left sticky-top">
                 <h3>
                   Grow Your Professional Career with{" "}
-                  <span class="span222">Top-Rated</span> Mentors
+                  <span className="span222">Top-Rated</span> Mentors
                 </h3>
 
                 <p>
@@ -71,51 +72,51 @@ const LoginForm = () => {
                   work under pressure on various projects within tight
                   deadlines. At Practiwiz we have courses for working
                   professionals, MBA students, and aspiring IT business
-                  analysts. Hurry up and reserve your mentorship class today.
+                  analysts. Hurry up and reserve your mentorship className today.
                 </p>
 
-                <h5 class="mt-4">Benefits of Our Mentorship Course:</h5>
+                <h5 className="mt-4">Benefits of Our Mentorship Course:</h5>
 
-                <ul class="ps-0 mt-3">
-                  <li class="mb-3">
-                    <div class="d-flex align-items-center">
-                      <i class="fa-solid fa-circle-check"></i>
+                <ul className="ps-0 mt-3">
+                  <li className="mb-3">
+                    <div className="d-flex align-items-center">
+                      <i className="fa-solid fa-circle-check"></i>
 
-                      <p class="mb-0">Self-paced training</p>
+                      <p className="mb-0">Self-paced training</p>
                     </div>
                   </li>
 
-                  <li class="mb-3">
-                    <div class="d-flex align-items-center">
-                      <i class="fa-solid fa-circle-check"></i>
+                  <li className="mb-3">
+                    <div className="d-flex align-items-center">
+                      <i className="fa-solid fa-circle-check"></i>
 
-                      <p class="mb-0">Flexible timing and scheduling</p>
+                      <p className="mb-0">Flexible timing and scheduling</p>
                     </div>
                   </li>
 
-                  <li class="mb-3">
-                    <div class="d-flex align-items-center">
-                      <i class="fa-solid fa-circle-check"></i>
+                  <li className="mb-3">
+                    <div className="d-flex align-items-center">
+                      <i className="fa-solid fa-circle-check"></i>
 
-                      <p class="mb-0">
+                      <p className="mb-0">
                         Career guidance from experienced mentors
                       </p>
                     </div>
                   </li>
 
-                  <li class="mb-3">
-                    <div class="d-flex align-items-center">
-                      <i class="fa-solid fa-circle-check"></i>
+                  <li className="mb-3">
+                    <div className="d-flex align-items-center">
+                      <i className="fa-solid fa-circle-check"></i>
 
-                      <p class="mb-0">Expert advice and guidance</p>
+                      <p className="mb-0">Expert advice and guidance</p>
                     </div>
                   </li>
 
-                  <li class="mb-3">
-                    <div class="d-flex align-items-center">
-                      <i class="fa-solid fa-circle-check"></i>
+                  <li className="mb-3">
+                    <div className="d-flex align-items-center">
+                      <i className="fa-solid fa-circle-check"></i>
 
-                      <p class="mb-0">
+                      <p className="mb-0">
                         <a href="/register">Sign Up Now!</a>
                       </p>
                     </div>
@@ -124,76 +125,78 @@ const LoginForm = () => {
               </div>
             </div>
 
-            <div class="col-lg-6">
-              <div class="iuhieiuihaw_right bg-white p-5">
-                <div class="uherrr_text text-center">
+            <div className="col-lg-6">
+              <div className="iuhieiuihaw_right bg-white p-5">
+                <div className="uherrr_text text-center">
                   <h4>Log in</h4>
 
-                  <p class="mb-0">
+                  <p className="mb-0">
                     Do Not Have An Account? <a href="#">Sign Up</a>
                   </p>
                 </div>
 
-                <div class="ihduwfr_form_wrapper mt-3">
-                  <form>
-                    <div class="csfvgdtrfs mb-3 position-relative">
-                      <label for="exampleInputEmail1" class="form-label">
+                <div className="ihduwfr_form_wrapper mt-3">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="csfvgdtrfs mb-3 position-relative">
+                      <label htmlFor="exampleInputEmail1" className="form-label">
                         Email
                       </label>
                       <input
                         type="email"
-                        class="form-control"
-                        id="exampleInputEmail1"
+                        className="form-control"
+                        // id="exampleInputEmail1"
                         placeholder="Email"
-                        aria-describedby="emailHelp"
+                        // aria-describedby="emailHelp"
+                        {...register("email")}
                       />
 
-                      <i class="fa-solid fa-envelopes-bulk position-absolute"></i>
+                      <i className="fa-solid fa-envelopes-bulk position-absolute"></i>
                     </div>
 
-                    <div class="csfvgdtrfs mb-3 position-relative">
-                      <label for="exampleInputEmail1" class="form-label">
+                    <div className="csfvgdtrfs mb-3 position-relative">
+                      <label htmlFor="exampleInputEmail1" className="form-label">
                         Password
                       </label>
                       <input
                         type="email"
-                        class="form-control"
+                        className="form-control"
                         id="exampleInputEmail1"
                         placeholder="Password must be at least 8 characters"
                         aria-describedby="emailHelp"
+                        {...register("password")}
                       />
 
-                      <i class="fa-solid fa-eye position-absolute"></i>
+                      <i className="fa-solid fa-eye position-absolute"></i>
                     </div>
 
-                    <a href="#" class="uidherrrr_anchor">
+                    <a href="#" className="uidherrrr_anchor">
                       Forget password?
                     </a>
 
-                    <button type="submit" class="btn btn-main py-3 mt-4">
+                    <button type="submit" className="btn btn-main py-3 mt-4">
                       Log in
                     </button>
-
-                    <div class="digheirer text-center pt-3 pb-2">
-                      <h4 class="mb-0" style={{ fontSize: "1.2rem" }}>
+                    </form>
+                    <div className="digheirer text-center pt-3 pb-2">
+                      <h4 className="mb-0" style={{ fontSize: "1.2rem" }}>
                         <b>OR</b>
                       </h4>
                     </div>
 
-                    <div class="dieyhr_iuhfiderr mt-2">
-                      <div class="d-flex align-items-center">
-                        <div class="btn btn-main">
+                    <div className="dieyhr_iuhfiderr mt-2">
+                      <div className="d-flex align-items-center">
+                        <div className="btn btn-main">
                           <img
-                            class="me-1"
+                            className="me-1"
                             src="images/facebooklog.webp"
                             alt=""
                           />{" "}
                           Facebook
                         </div>
 
-                        <div class="btn btn-main">
+                        <div className="btn btn-main">
                           <img
-                            class="me-1"
+                            className="me-1"
                             src="images/googlelog.webp"
                             alt=""
                           />{" "}
@@ -201,7 +204,7 @@ const LoginForm = () => {
                         </div>
                       </div>
                     </div>
-                  </form>
+                  {/* </form> */}
                 </div>
               </div>
             </div>
--- src/Components/Forms/Register/Mentor/MentorForm2.js
+++ src/Components/Forms/Register/Mentor/MentorForm2.js
@@ -1,133 +1,147 @@
 import React from "react";
+import { useFormContext } from 'react-hook-form';
 
 const MentorForm2 = () => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
   return (
-    <div class="doiherner_wrapper">
-      <div class="ihduwfr_form_wrapper p-0" style={{ height: "auto" }}>
-        <div class="row">
-          <div class="col-lg-6">
-            <div class="mb-4">
-              <label for="exampleInputEmail1" class="form-label">
+    <div className="doiherner_wrapper">
+      <div className="ihduwfr_form_wrapper p-0" style={{ height: "auto" }}>
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="mb-4">
+              <label htmlFor="exampleInputEmail1" className="form-label">
                 <b>Job Title</b>
               </label>
               <input
                 type="email"
-                class="form-control"
-                id="exampleInputEmail1"
+                className="form-control"
+                // id="exampleInputEmail1"
                 placeholder="Type Your Job Title"
                 aria-describedby="emailHelp"
+                {...register("mentor_job_title", {
+                  // required: "First Name is required",
+                })} //1
               />
             </div>
-            <div class="mb-4">
-              <label for="exampleInputPassword1" class="form-label">
+            <div className="mb-4">
+              <label htmlFor="exampleInputPassword1" className="form-label">
                 <b>Years of Experience</b>
               </label>
               <input
                 type="password"
-                class="form-control"
-                id="exampleInputPassword1"
+                className="form-control"
+                // id="exampleInputPassword1"
                 placeholder="Your Experience"
+                {...register("years_of_experience", {
+                  // required: "First Name is required",
+                })} //1
               />
             </div>
 
-            <div class="mb-4">
-              <label for="exampleInputEmail1" class="form-label">
+            <div className="mb-4">
+              <label htmlFor="exampleInputEmail1" className="form-label">
                 <b>Company</b>
               </label>
               <input
                 type="email"
-                class="form-control"
-                id="exampleInputEmail1"
+                className="form-control"
+                // id="exampleInputEmail1"
                 placeholder="Type Your Company Name"
                 aria-describedby="emailHelp"
+                {...register("mentor_company_name", {
+                  // required: "First Name is required",
+                })} //1
               />
             </div>
           </div>
 
-          <div class="col-lg-6">
-            <div class="ikhwnjrr_right">
-              <label for="exampleInputEmail1" class="form-label mb-3">
+          <div className="col-lg-6">
+            <div className="ikhwnjrr_right">
+              <label htmlFor="exampleInputEmail1" className="form-label mb-3">
                 <b>Percentage Completion</b>
               </label>
 
-              <div class="d-flex align-items-center">
-                <div class="hinrer_circle position-relative me-3">
+              <div className="d-flex align-items-center">
+                <div className="hinrer_circle position-relative me-3">
                   <h2>SK</h2>
                 </div>
 
-                <div class="idhnerier_right">
-                  <h4 class="mb-1">Sawan Kumar</h4>
+                <div className="idhnerier_right">
+                  <h4 className="mb-1">Sawan Kumar</h4>
 
-                  <p class="mb-1">
+                  <p className="mb-1">
                     <b>40% Complete</b>
                   </p>
 
-                  <h6 class="mb-0">Signed up - 4 minutes ago</h6>
+                  <h6 className="mb-0">Signed up - 4 minutes ago</h6>
                 </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div class="row align-items-center">
-          <div class="col-lg-7 mb-4">
-            <label for="exampleInputEmail1" class="form-label">
+        <div className="row align-items-center">
+          <div className="col-lg-7 mb-4">
+            <label htmlFor="exampleInputEmail1" className="form-label">
               <b>Passionate About!</b> (Select max of 4 options)
             </label>
-            <div id="container" class="bg-white"></div>
+            <div type="" id="container" className="bg-white"></div>
 
-            <p class="iduehnbriee_text mb-0">
+            <p className="iduehnbriee_text mb-0">
               (*Drag and drop the most suitable option in the box*)
             </p>
           </div>
 
-          <div class="col-lg-5 mb-4">
+          <div className="col-lg-5 mb-4">
             <div id="outside-container">
-              <div class="draggable" id="draggable1" draggable="true">
+              <div className="draggable" id="draggable1" draggable="true">
                 Technology
                 <span
-                  class="close-btn"
-                  onclick="removeFromContainer('draggable1')"
+                  className="close-btn"
+                  // onclick="removeFromContainer('draggable1')"
                 >
                   &times;
                 </span>
               </div>
 
-              <div class="draggable" id="draggable2" draggable="true">
+              <div className="draggable" id="draggable2" draggable="true">
                 Management
                 <span
-                  class="close-btn"
-                  onclick="removeFromContainer('draggable2')"
+                  className="close-btn"
+                  // onclick="removeFromContainer('draggable2')"
                 >
                   &times;
                 </span>
               </div>
 
-              <div class="draggable" id="draggable3" draggable="true">
+              <div className="draggable" id="draggable3" draggable="true">
                 Leadership
                 <span
-                  class="close-btn"
-                  onclick="removeFromContainer('draggable3')"
+                  className="close-btn"
+                  // onClick="removeFromContainer('draggable3')"
                 >
                   &times;
                 </span>
               </div>
 
-              <div class="draggable" id="draggable4" draggable="true">
+              <div className="draggable" id="draggable4" draggable="true">
                 Career Guidance
                 <span
-                  class="close-btn"
-                  onclick="removeFromContainer('draggable4')"
+                  className="close-btn"
+                  // onClick="removeFromContainer('draggable4')"
                 >
                   &times;
                 </span>
               </div>
 
-              <div class="draggable" id="draggable5" draggable="true">
+              <div className="draggable" id="draggable5" draggable="true">
                 Public Speaking
                 <span
-                  class="close-btn"
-                  onclick="removeFromContainer('draggable5')"
+                  className="close-btn"
+                  // onClick="removeFromContainer('draggable5')"
                 >
                   &times;
                 </span>
@@ -136,22 +150,25 @@ const MentorForm2 = () => {
           </div>
         </div>
 
-        <div class="row">
-          <div class="col-lg-12 mb-4">
-            <label for="exampleInputEmail1" class="form-label mb-0">
+        <div className="row">
+          <div className="col-lg-12 mb-4">
+            <label htmlFor="exampleInputEmail1" className="form-label mb-0">
               <b>Areas of Expertise</b>
             </label>
 
-            <div class="moideuirer_list">
-              <ul class="ps-0 mb-0">
+            <div className="moideuirer_list">
+              <ul className="ps-0 mb-0">
                 <li>
                   <input
                     type="checkbox"
                     id="check_1"
                     name="check_1"
                     value="check_1"
+                    {...register("areas_of_expertise", {
+                      // required: "First Name is required",
+                    })} //1
                   />
-                  <label for="check_1">Agile</label>
+                  <label htmlFor="check_1">Agile</label>
                 </li>
 
                 <li>
@@ -160,8 +177,11 @@ const MentorForm2 = () => {
                     id="check_2"
                     name="check_2"
                     value="check_2"
+                    {...register("areas_of_expertise", {
+                      // required: "First Name is required",
+                    })} //1
                   />
-                  <label for="check_2">AI</label>
+                  <label htmlFor="check_2">AI</label>
                 </li>
 
                 <li>
@@ -170,8 +190,11 @@ const MentorForm2 = () => {
                     id="check_3"
                     name="check_3"
                     value="check_3"
+                    {...register("areas_of_expertise", {
+                      // required: "First Name is required",
+                    })} //1
                   />
-                  <label for="check_3">Cloud</label>
+                  <label htmlFor="check_3">Cloud</label>
                 </li>
 
                 <li>
@@ -180,8 +203,11 @@ const MentorForm2 = () => {
                     id="check_4"
                     name="check_4"
                     value="check_4"
+                    {...register("areas_of_expertise", {
+                      // required: "First Name is required",
+                    })} //1
                   />
-                  <label for="check_4">Python</label>
+                  <label htmlFor="check_4">Python</label>
                 </li>
 
                 <li>
@@ -190,8 +216,11 @@ const MentorForm2 = () => {
                     id="check_5"
                     name="check_5"
                     value="check_5"
+                    {...register("areas_of_expertise", {
+                      // required: "First Name is required",
+                    })} //1
                   />
-                  <label for="check_5">DBA</label>
+                  <label htmlFor="check_5">DBA</label>
                 </li>
 
                 <li>
@@ -200,8 +229,11 @@ const MentorForm2 = () => {
                     id="check_6"
                     name="check_6"
                     value="check_6"
+                    {...register("areas_of_expertise", {
+                      // required: "First Name is required",
+                    })} //1
                   />
-                  <label for="check_6">JAVA</label>
+                  <label htmlFor="check_6">JAVA</label>
                 </li>
 
                 <li>
@@ -210,8 +242,11 @@ const MentorForm2 = () => {
                     id="check_7"
                     name="check_7"
                     value="check_7"
+                    {...register("areas_of_expertise", {
+                      // required: "First Name is required",
+                    })} //1
                   />
-                  <label for="check_7">Selenium</label>
+                  <label htmlFor="check_7">Selenium</label>
                 </li>
 
                 <li>
@@ -220,8 +255,11 @@ const MentorForm2 = () => {
                     id="check_8"
                     name="check_8"
                     value="check_8"
+                    {...register("areas_of_expertise", {
+                      // required: "First Name is required",
+                    })} //1
                   />
-                  <label for="check_8">Conflict Resolution</label>
+                  <label htmlFor="check_8">Conflict Resolution</label>
                 </li>
 
                 <li>
@@ -230,8 +268,11 @@ const MentorForm2 = () => {
                     id="check_9"
                     name="check_9"
                     value="check_9"
+                    {...register("areas_of_expertise", {
+                      // required: "First Name is required",
+                    })} //1
                   />
-                  <label for="check_9">Communication</label>
+                  <label htmlFor="check_9">Communication</label>
                 </li>
 
                 <li>
@@ -240,30 +281,36 @@ const MentorForm2 = () => {
                     id="check_10"
                     name="check_10"
                     value="check_10"
+                    {...register("areas_of_expertise", {
+                      // required: "First Name is required",
+                    })} //1
                   />
-                  <label for="check_10">Resume Writing</label>
+                  <label htmlFor="check_10">Resume Writing</label>
                 </li>
               </ul>
             </div>
           </div>
         </div>
 
-        <div class="row">
-          <div class="col-lg-12 mb-4">
-            <label for="exampleInputEmail1" class="form-label mb-0">
+        <div className="row">
+          <div className="col-lg-12 mb-4">
+            <label htmlFor="exampleInputEmail1" className="form-label mb-0">
               <b>Academic Qualification</b>
             </label>
 
-            <div class="dkjiherer moideuirer_list">
-              <ul class="ps-0 mb-0">
+            <div className="dkjiherer moideuirer_list">
+              <ul className="ps-0 mb-0">
                 <li>
                   <input
                     type="checkbox"
                     id="check_11"
                     name="check_11"
                     value="check_11"
+                    {...register("academic_qualification", {
+                      // required: "First Name is required",
+                    })} //1
                   />
-                  <label for="check_11">Post Graduate</label>
+                  <label htmlFor="check_11">Post Graduate</label>
                 </li>
 
                 <li>
@@ -272,8 +319,11 @@ const MentorForm2 = () => {
                     id="check_20"
                     name="check_20"
                     value="check_20"
+                    {...register("academic_qualification", {
+                      // required: "First Name is required",
+                    })} //1
                   />
-                  <label for="check_20">Graduate</label>
+                  <label htmlFor="check_20">Graduate</label>
                 </li>
 
                 <li>
@@ -282,33 +332,42 @@ const MentorForm2 = () => {
                     id="check_30"
                     name="check_30"
                     value="check_30"
+                    {...register("academic_qualification", {
+                      // required: "First Name is required",
+                    })} //1
                   />
-                  <label for="check_30">Doctorate</label>
+                  <label htmlFor="check_30">Doctorate</label>
                 </li>
               </ul>
             </div>
           </div>
 
-          <div class="col-lg-12 mb-4">
-            <label for="exampleInputEmail1" class="form-label">
+          <div className="col-lg-12 mb-4">
+            <label htmlFor="exampleInputEmail1" className="form-label">
               <b>Your Recommended Area of Mentorship</b>
             </label>
             <textarea
-              class="form-control"
+              className="form-control"
               style={{ height: "150px" }}
+              {...register("recommended_area_of_mentorship", {
+                // required: "First Name is required",
+              })} //1
             ></textarea>
           </div>
 
-          <div class="col-lg-12 mb-4">
-            <label for="exampleInputEmail1" class="form-label">
+          <div className="col-lg-12 mb-4">
+            <label htmlFor="exampleInputEmail1" className="form-label">
               <b>Headline</b>
             </label>
             <input
               type="email"
-              class="form-control"
+              className="form-control"
               id="exampleInputEmail1"
               placeholder="Type A Headline Here"
               aria-describedby="emailHelp"
+              {...register("mentor_Headline", {
+                // required: "First Name is required",
+              })} //1
             />
           </div>
         </div>
